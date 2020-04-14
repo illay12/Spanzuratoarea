2ff190766ffd39f6bf04055007d69067ec421235
Laurik <laurentiupetrica11@yahoo.com>
2020-04-14 11:37:27 +0300

    Spanzuratorea v2.0

---
+++ spanz_GUI.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+
+
+Height = 800
+Width = 1200
+
+root = tk.Tk()
+
+canvas = tk.Canvas(root,height=Height,width=Width)
+canvas.pack()
+
+
+
+root.mainloop()
--- spanzuratoarea.py
+++ spanzuratoarea.py
@@ -7,8 +7,11 @@ class Spanzuratorea:
         self.aux_word = []
         self.tries = 3
         self.game_active = True
+        self.rounds = 1
 
     def game(self):
+        print("Welcome to Spanzuratoarea\nIn this game you have to guess a word letter by letter\n"
+              "You only have 3 tries so Good luck!")
         while self.game_active:
             self.play()
 
@@ -19,27 +22,40 @@ class Spanzuratorea:
         while True:
             print(*self.word_displayed)
             letter = input("Enter a letter:")
-            if letter in self.word_displayed:
+            if len(letter) > 1:
+                print("Please enter just a single letter.")
+            elif letter in self.word_displayed:
                 print(f"You have already entered the letter {letter}")
             elif letter in self.word:
                 self._fit_letter(letter)
             else:
                 print("Try again!")
-            if self._check_win():
-                choice = input("Want to play again?'y' for yes and 'n' for no.\n")
-                if choice.lower() == 'y':
-                    break
-                elif choice.lower() == 'n':
-                    print("Thanks for playing!")
-                    self.game_active = False
-                    break
+                self.tries -= 1
+                print("Tries left: {}".format(self.tries))
+            if self._check_win_lose():
+                self._play_again()
+                break
 
+    def _play_again(self):
+        choice = input("Want to play again?'y' for yes and 'n' for no.\n")
+        while choice.lower() != 'y' and choice.lower() != 'n':
+            choice = input("Please enter a suitable character:\n")
+        if choice.lower() == 'y':
+            self.rounds += 1
+            print(f"Beggining Round {self.rounds}\n")
+            self.tries = 3
+        elif choice.lower() == 'n':
+            print("Thanks for playing!")
+            self.game_active = False
 
-    def _check_win(self):
+    def _check_win_lose(self):
         """Checks if the word has been guessed."""
         if self.word_displayed == list(self.word):
-            print(f"{self.word} was the word.")
+            print(f"'{self.word}' was the word.")
             print("You won!!")
+            return True
+        elif self.tries <= 0:
+            print(f"'{self.word}' was the word.")
             return True
 
     def _fit_letter(self,letter):
@@ -54,8 +70,15 @@ class Spanzuratorea:
         self.word = input("Enter a word that someone has to guess:\n")
         self.word_displayed = list(len(self.word) * '_')
         self.aux_word = list(self.word)
+        self._convert_spaces()
         print("Try to guess the word by entering a letter that could fit.")
 
+    def _convert_spaces(self):
+        while " " in self.aux_word:
+            index = self.aux_word.index(" ")
+            self.word_displayed[index] = " "
+            self.aux_word[index] = "_"
+            print(self.aux_word)
 
 sp = Spanzuratorea()
 sp.game()
